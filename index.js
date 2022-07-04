eb5c191f5d408d610cf05a37554654fa75ddc38c
zhangxin <heermosi39@163.com>
2022-07-05 00:51:59 +0800

    fix: 优化

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const HEADERS = {
   "sec-fetch-dest": "empty",
   "sec-fetch-mode": "cors",
   "sec-fetch-site": "same-site",
-  "cookie": fs.readFileSync(__dirname + '/cookie.txt', 'utf-8'),
+  "cookie": "acw_tc=276077b016569504139411468e4440a55de6cfc7e3da04684ca4f2fb97dd33; xq_a_token=fb258a8752383013c5b5b5492aea4de18508044c; xqat=fb258a8752383013c5b5b5492aea4de18508044c; xq_r_token=9a89a07d59400026c830b71209c77f8f44b2824e; xq_id_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1aWQiOi0xLCJpc3MiOiJ1YyIsImV4cCI6MTY1OTIyMzk0MiwiY3RtIjoxNjU2OTUwMzgxMzQ0LCJjaWQiOiJkOWQwbjRBWnVwIn0.GWR3wKTlcrvMn5wZyNO2ntceiAhqyGLWaJCpfRRmUUvzqZ0CPNvZF_SYwYf251WOBM7RXe1IBRsfl95ARJNtfUAgCEclIU3TJ_N7Oaia2kgVSoKOq9cfwfoII4TcloZtiUEEqEz5ZWeGhYvQNBJ9M3KGld2RgweHbMlLqp5XKQK2hePLaIUD6nD21dns9AckWMFKTdTzd_06_wtr7wBDFVVQfvY6X0Rq4WhaEmqkgKPKAMNwwWY_fBoY7ghD5iA-wJonQJobHiBjyW9CjEHG1pRMwUjmOSa6A_4v3ZuCwadUfTTZy4PCqButbGJ2ZaF2yjy81Y-ShVu3ZJDsaguyww; u=301656950413949; is_overseas=0; Hm_lvt_1db88642e346389874251b5a1eded6e3=1654923205; Hm_lpvt_1db88642e346389874251b5a1eded6e3=1656950419; device_id=fdc58ff81c7f9adfb645308188f8364a",
   "Referer": "https://xueqiu.com/S/CSI931151",
 };
 const BEFORE_URL = 'https://stock.xueqiu.com/v5/stock/chart/kline.json';
@@ -129,6 +129,7 @@ app.use(
               const key = resData.column[index];
               if (key) {
                 if (key === 'timestamp') {
+                  total.originTimestamp = val;
                   total[key] = dayjs(val).format('YYYY/MM/DD')
                 } else {
                   total[key] = val
@@ -137,11 +138,23 @@ app.use(
               return total;
             }, {})
           })
+          const curWeekStart = dayjs().startOf('week').toDate().getTime();
+          let nearLessItem = null;
+          const curWeekItems = infoItems.filter((item, index) => {
+            const isCurWeek = item.originTimestamp >= curWeekStart;
+            if (isCurWeek && nearLessItem === null && index > 0) {
+              nearLessItem = infoItems[index - 1];
+            }
+            return isCurWeek;
+          });
+
           const curNode = infoItems.splice(-1)[0];
           const beforeTimeSlot = `${infoItems[1].timestamp} - ${infoItems[infoItems.length - 1].timestamp}`;
           const beforeRate = getRate(infoItems[infoItems.length - 1].close, infoItems[0].close);
           const allRate = getRate(curNode.close, infoItems[0].close);
           const fiveRate = getRate(curNode.close, infoItems[5].close);
+          const curWeekRate = nearLessItem && curWeekItems.length && getRate(curWeekItems[curWeekItems.length-1].close, nearLessItem.close);
+
           const result = {
             name: item.name,
             code: resData.symbol,
@@ -150,11 +163,12 @@ app.use(
             allRate,
             fiveRate,
             curRate: curNode.percent,
+            curWeekRate: curWeekRate || 0,
           };
 
           return result;
         } catch (e) {
-          console.log('e', item.name)
+          console.log('e', item.name, e)
         }
       }))
       result.sort((a, b) => b && a && (b.allRate - a.allRate));
@@ -166,6 +180,7 @@ app.use(
         item.beforeRate = `${((item.beforeRate) * 100).toFixed(2)}%`;
         item.fiveRate = `${((item.fiveRate) * 100).toFixed(2)}%`;
         item.curRate = `${item.curRate}%`;
+        item.curWeekRate = `${((item.curWeekRate) * 100).toFixed(2)}%`;
         return item;
       })
   })
@@ -176,6 +191,8 @@ app.use(
     let originalUrl = ctx && ctx.originalUrl;
     if (!originalUrl || originalUrl === '/') {
       originalUrl = '/index.html'
+    } else {
+      ctx.set('cache-control', 'public, max-age=31536000');
     }
     ctx.status = 200;
     ctx.body = fs.readFileSync(__dirname + originalUrl, 'utf-8')
